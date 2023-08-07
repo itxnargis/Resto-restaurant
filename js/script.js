92de8b977c4bfbcce976a832e017135d11dadf30
itxnargis <itxnargiskhatun@gmail.com>
2023-08-07 16:15:28 +0530

    feat - Add review section

--- js/script.js
+++ js/script.js
@@ -2,21 +2,21 @@ let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
 
-window.onscroll = () =>{
+window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
 
-document.querySelector('#search-icon').onclick = () =>{
+document.querySelector('#search-icon').onclick = () => {
     document.querySelector('#search-form').classList.toggle('active');
 }
 
-document.querySelector('#close').onclick = () =>{
+document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
 }
 
@@ -24,12 +24,40 @@ var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
+        delay: 7500,
+        disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     loop: true,
-    });
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    breakpoints: {
+        0: {
+            slidesPerView:1,
+        },
+        640: {
+            slidesPerView:2,
+        },
+        768: {
+            slidesPerView:2,
+        },
+        1024: {
+            slidesPerView:3,
+        },
+    }
+});
