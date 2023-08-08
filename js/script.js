ce83c2e2529722d936136971f7204fd980a9eff3
itxnargis <itxnargiskhatun@gmail.com>
2023-08-08 11:05:41 +0530

    feat - Add footer section

--- js/script.js
+++ js/script.js
@@ -7,9 +7,28 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec => {
+
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if(top => offset && top < offset + height){
+            navLinks.link.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+            });
+        };
+
+    });
 }
 
 document.querySelector('#search-icon').onclick = () => {
@@ -61,3 +80,12 @@ var swiper = new Swiper(".review-slider", {
         },
     }
 });
+// function loader(){
+//     document.querySelector('.loader-container').classList.add('fade-out');
+// }
+
+// function fadeOut(){
+//     setInterval(loader, 3000);
+// }
+
+// window.onload = fadeOut;
